aea2291fb1c5b6eb6b9270b4a6a2ff2ed477024a
Victoria <Ponomarevaviki@mail.ru>
2017-11-27 19:25:49 +0600

    Сделаны задачи 1-4 в лабораторной 1

---
+++ src/lab1/lab2.java
@@ -0,0 +1,13 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class lab2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String a;
+        a=s.nextLine();
+        System.out.println("Мы стремимся к "+a);
+
+    }
+}
---
+++ src/lab1/lab3.java
@@ -0,0 +1,13 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class lab3 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String a;
+        a=s.nextLine();
+        System.out.println(a+" нас не остановят");
+
+    }
+}
